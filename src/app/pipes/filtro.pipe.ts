076abce873f718f583d3f4ff95b7c72643371981
= <sbouzoubaa@uoc.edu>
2021-04-25 13:22:18 +0200

    commit

--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -19,3 +19,18 @@ export class FiltroPipe implements PipeTransform {
   }
 }
 
+
+//Filtro realizdo por Marcos:
+
+// transform(value: any, arg: any): any {
+//   if (arg === '')
+//   return value;
+//   const songResult = [];
+//   for (const song of value) {
+//     if (song.title.toLowerCase().startsWith(arg.toLowerCase()) || song.performer.toString().toLowerCase().startsWith(arg.toLowerCase()) ) {
+//       songResult.push(song);
+
+//     }
+//   }
+//   return songResult;
+// }
