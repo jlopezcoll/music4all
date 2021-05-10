6e6ccf75f8b8f902bf74331fa8444d033697ad8d
Marcos López Collar <jlopezcoll@uoc.edu>
2021-05-11 01:33:46 +0200

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,31 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: '<your-key>',
+//     authDomain: '<your-project-authdomain>',
+//     databaseURL: '<your-database-URL>',
+//     projectId: '<your-project-id>',
+//     storageBucket: '<your-storage-bucket>',
+//     messagingSenderId: '<your-messaging-sender-id>',
+//     appId: '<your-app-id>',
+//     measurementId: '<your-measurement-id>'
+//   }
+// };
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBwHS57ahusU-MJriq5mlzSAICeDc4VxWA",
+    authDomain: "music4all-8cfdb.firebaseapp.com",
+    projectId: "music4all-8cfdb",
+    storageBucket: "music4all-8cfdb.appspot.com",
+    messagingSenderId: "747122516654",
+    appId: "1:747122516654:web:be98f9f26e813515cac33f",
+    measurementId: "G-9RQRLLMBMB"
+  }
 };
 
 /*
